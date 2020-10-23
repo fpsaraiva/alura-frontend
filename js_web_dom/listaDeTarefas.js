fed930d2042c1c957a9264dcb166e5166e24edd3
Fernando Saraiva <fpsaraiva@gmail.com>
2020-10-23 12:54:40 -0300

    exercicios aulas 03 e 04

--- js_web_dom/listaDeTarefas.js
+++ js_web_dom/listaDeTarefas.js
@@ -1,11 +1,42 @@
-const criarTarefa = function(evento) {
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const valor = input.value;
-  console.log(valor);
-  input.value = "";
-}
-
-const novaTarefa = document.querySelector('[data-form-button]');
-
-novaTarefa.addEventListener('click', criarTarefa);
+(() => {
+  const criarTarefa = function(evento) {
+    evento.preventDefault();
+    
+    const lista = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+  
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+  
+    const conteudo = `<p class="content">${valor}</p>`;
+  
+    tarefa.innerHTML = conteudo;
+  
+    tarefa.appendChild(BotaoConclui());
+    lista.appendChild(tarefa);
+    input.value = "";
+  }
+  
+  const novaTarefa = document.querySelector('[data-form-button]');
+  
+  novaTarefa.addEventListener('click', criarTarefa);
+  
+  const BotaoConclui = function() {
+    const botaoConclui = document.createElement('button');
+  
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerHTML = 'concluir';
+    botaoConclui.addEventListener('click', concluirTarefa);
+  
+    return botaoConclui;
+  }
+  
+  const concluirTarefa = function(evento) {
+    const botaoConclui = evento.target;
+  
+    const tarefaCompleta = botaoConclui.parentElement;
+  
+    tarefaCompleta.classList.toggle('done');
+  }
+})();
