2289158291550d03384a371a1c7d59a4937868a9
Fernando Saraiva <fpsaraiva@gmail.com>
2020-10-27 20:46:17 -0300

    exercicios aulas 01, 02 e 03

---
+++ js_linguagem_web/js/principal.js
@@ -0,0 +1,40 @@
+const titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if(peso < 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if(altura < 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if(pesoEhValido && alturaEhValida) {
+    tdImc.textContent = (peso / (altura * altura)).toFixed(2);
+  } else {
+    //tdImc.textContent = "Altura e/ou peso inválidos!"
+  }
+
+}
